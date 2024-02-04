bc28df4e246154a8d361f538b3082de17bef2b0c
nick <62725534+nicklvh@users.noreply.github.com>
2024-02-04 17:06:23 +0000

    chore: major cleanup of index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
 import "@sapphire/plugin-logger/register";
-import "@sapphire/plugin-api/register";
 
 import { PrismaClient } from "@prisma/client";
 import {
   ApplicationCommandRegistries,
-  LogLevel,
   RegisterBehavior,
   SapphireClient,
   container,
 } from "@sapphire/framework";
-import { GatewayIntentBits, OAuth2Scopes, Partials } from "discord.js";
-import { ModerationManager } from "#classes/index";
-import { envParseNumber, envParseString, setup } from "@skyra/env-utilities";
-import { join } from "path";
+import { GatewayIntentBits } from "discord.js";
 
-process.env.NODE_ENV ??= "development";
-setup({ path: join(__dirname, "..", `.env.${process.env.NODE_ENV}.local`) });
+import { config } from "dotenv";
+config();
 
 async function start() {
   ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
-    RegisterBehavior.BulkOverwrite
+    RegisterBehavior.Overwrite
   );
 
   const client = new SapphireClient({
@@ -29,37 +24,22 @@ async function start() {
       GatewayIntentBits.GuildMessageReactions,
       GatewayIntentBits.GuildEmojisAndStickers,
     ],
-    partials: [Partials.GuildMember, Partials.Reaction, Partials.User],
-    logger: {
-      level:
-        process.env.NODE_ENV === "production" ? LogLevel.Info : LogLevel.Debug,
-    },
-    api: {
-      auth: {
-        id: envParseString("CLIENT_ID"),
-        cookie: envParseString("OAUTH_COOKIE"),
-        secret: envParseString("CLIENT_SECRET"),
-        scopes: [OAuth2Scopes.Identify, OAuth2Scopes.Guilds],
-        redirect: envParseString("OAUTH_REDIRECT"),
-      },
-      prefix: envParseString("API_PREFIX"),
-      origin: envParseString("API_ORIGIN"),
-      listenOptions: {
-        port: envParseNumber("API_PORT", 3000),
-      },
-    },
   });
 
   await client.login();
 
   const prisma = new PrismaClient();
   container.prisma = prisma;
+
   await prisma.$connect().then(() => {
     container.logger.info("Connected to Database successfully!");
   });
-
-  const moderationManager = new ModerationManager();
-  container.moderationManager = moderationManager;
 }
 
 void start();
+
+declare module "@sapphire/pieces" {
+  interface Container {
+    prisma: PrismaClient;
+  }
+}
