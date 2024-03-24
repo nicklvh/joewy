67a8105aa33a89f28b70e1ced0689f2fac1afee2
nick <62725534+nicklvh@users.noreply.github.com>
2024-03-24 14:52:56 +0000

    feat(auditlog): update roleUpdate
    this update makes it more informative and does checks instead of throwing every detail at you

--- src/listeners/guild/roleUpdate.ts
+++ src/listeners/guild/roleUpdate.ts
@@ -22,11 +22,26 @@ export class RoleUpdateListener extends Listener {
           )} (\`${newRole.id}\`)`,
           inline: true,
         },
+      ])
+      .setColor(newRole.hexColor);
+
+    const colorChanged = newRole.hexColor === oldRole.hexColor ? false : true;
+    const hoistedChanged = newRole.hoist === oldRole.hoist ? false : true;
+    const mentionableChanged =
+      newRole.mentionable === oldRole.mentionable ? false : true;
+
+    if (colorChanged) {
+      embed.addFields([
         {
           name: "Color",
           value: `Old Color: ${oldRole.hexColor} New Color: ${newRole.hexColor}`,
           inline: true,
         },
+      ]);
+    }
+
+    if (hoistedChanged) {
+      embed.addFields([
         {
           name: "Hoisted",
           value: `Hoisted Before: ${
@@ -34,6 +49,11 @@ export class RoleUpdateListener extends Listener {
           } Hoisted After: ${newRole.hoist ? "`Yes`" : "`No`"}`,
           inline: true,
         },
+      ]);
+    }
+
+    if (mentionableChanged) {
+      embed.addFields([
         {
           name: "Mentionable",
           value: `Mentionable Before: ${
@@ -41,8 +61,41 @@ export class RoleUpdateListener extends Listener {
           } Mentionable After: ${newRole.mentionable ? "`Yes`" : "`No`"}`,
           inline: true,
         },
-      ])
-      .setColor(newRole.hexColor);
+      ]);
+    }
+
+    const permsRemoved: string[] = [];
+    const permsAdded: string[] = [];
+
+    oldRole.permissions.toArray().forEach((perm) => {
+      if (!newRole.permissions.has(perm)) {
+        permsRemoved.push(perm);
+      }
+
+      if (!oldRole.permissions.has(perm)) {
+        permsAdded.push(perm);
+      }
+    });
+
+    if (permsRemoved.length) {
+      embed.addFields([
+        {
+          name: "Permissions Removed",
+          value: permsRemoved.join(", "),
+          inline: true,
+        },
+      ]);
+    }
+
+    if (permsAdded.length) {
+      embed.addFields([
+        {
+          name: "Permissions Added",
+          value: permsAdded.join(", "),
+          inline: true,
+        },
+      ]);
+    }
 
     return channel.send({
       embeds: [embed],
