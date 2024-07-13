f72a059389122db5cf12a13aeac9bd3efd48a412
nick <62725534+nicklvh@users.noreply.github.com>
2024-07-13 18:29:45 +0100

    chore: clean up

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,6 @@ import {
   container,
 } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
-import { Helpers } from "./classes/Helpers.js";
-import { Time } from "@sapphire/time-utilities";
 
 const client = new SapphireClient({
   intents: [
@@ -20,23 +18,20 @@ const client = new SapphireClient({
     GatewayIntentBits.GuildEmojisAndStickers,
   ],
   defaultCooldown: {
-    delay: Time.Second * 3, // 3000 milliseconds
+    delay: 3000, // 3 seconds
     limit: 2,
     scope: BucketScope.User,
   },
   logger: {
-    depth: LogLevel.Debug,
+    level: LogLevel.Debug,
   },
 });
 
-void client.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
 
 const prisma = new PrismaClient();
 container.prisma = prisma;
 
-const helpers = new Helpers();
-container.helpers = helpers;
-
-void prisma.$connect().then(() => {
+prisma.$connect().then(() => {
   container.logger.info("Connected to the database successfully!");
 });
